702cae6f3316a437a319f8941fbccae622dae4ee
winlogon <walker84837@gmail.com>
2025-01-15 01:41:35 +0100

    feat: update colors, add new recipes and balance

--- src/main/java/com/github/walker84837/CustomRecipes/Main.java
+++ src/main/java/com/github/walker84837/CustomRecipes/Main.java
@@ -1,9 +1,6 @@
 package com.github.walker84837.CustomRecipes;
 
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.text.format.TextColor;
-
 import net.kyori.adventure.text.minimessage.*;
 import net.kyori.adventure.text.minimessage.tag.standard.*;
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
@@ -28,6 +25,7 @@ import java.util.ArrayList;
 public class Main extends JavaPlugin implements Listener {
     ArrayList<ShapedRecipe> shapedRecipes = new ArrayList<>();
     ArrayList<FurnaceRecipe> smeltingRecipes = new ArrayList<>();
+    ArrayList<ShapelessRecipe> shapelessRecipes = new ArrayList<>();
 
     MiniMessage serializer = MiniMessage.builder()
      .tags(TagResolver.builder()
@@ -43,6 +41,13 @@ public class Main extends JavaPlugin implements Listener {
     public void onEnable() {
         shapedRecipes.add(saddleRecipe());
         shapedRecipes.add(breadRecipe());
+        shapedRecipes.add(nameTagRecipe());
+        shapedRecipes.add(compressedStoneRecipe());
+        shapedRecipes.add(compressedDirtRecipe());
+        shapedRecipes.add(compressedCobblestoneRecipe());
+        shapelessRecipes.add(decompressStoneRecipe());
+        shapelessRecipes.add(decompressDirtRecipe());
+        shapelessRecipes.add(decompressCobblestoneRecipe());
         smeltingRecipes.add(rottenFleshRecipe());
 
         for (ShapedRecipe recipe : shapedRecipes) {
@@ -50,6 +55,10 @@ public class Main extends JavaPlugin implements Listener {
         }
 
         for (FurnaceRecipe recipe : smeltingRecipes) {
+            getServer().addRecipe(recipe);
+        }
+
+        for (ShapelessRecipe recipe : shapelessRecipes) {
             getServer().addRecipe(recipe);
         }
 
@@ -66,6 +75,17 @@ public class Main extends JavaPlugin implements Listener {
         return saddleRecipe;
     }
 
+    private ShapedRecipe nameTagRecipe() {
+        NamespacedKey nameTagKey = new NamespacedKey(this, "nametag_item");
+        ItemStack result = new ItemStack(Material.NAME_TAG);
+        ShapedRecipe nameTagRecipe = new ShapedRecipe(nameTagKey, result);
+        nameTagRecipe.shape(" AC", " AA", "B  ");
+        nameTagRecipe.setIngredient('A', Material.STRING);
+        nameTagRecipe.setIngredient('B', Material.PAPER);
+        nameTagRecipe.setIngredient('C', Material.EMERALD);
+        return nameTagRecipe;
+    }
+
     private ShapedRecipe breadRecipe() {
         final String elementIdentifier = "super_toast";
 
@@ -73,7 +93,7 @@ public class Main extends JavaPlugin implements Listener {
         ItemStack breadItem = new ItemStack(Material.BREAD);
         ItemMeta meta = breadItem.getItemMeta();
         meta.itemName(
-            serializer.deserialize("<gradient:#5e4fa2:#f79459>Super Toast</gradient>")
+            serializer.deserialize("<gradient:#5e28fb:#af37fa>Super Toast</gradient>")
         );
         meta.getPersistentDataContainer().set(
             new NamespacedKey(this, elementIdentifier), PersistentDataType.STRING, elementIdentifier
@@ -84,7 +104,7 @@ public class Main extends JavaPlugin implements Listener {
         breadRecipe.shape("AAA", "BCB", "AAA");
         breadRecipe.setIngredient('A', Material.BREAD);
         breadRecipe.setIngredient('B', Material.WHEAT);
-        breadRecipe.setIngredient('C', Material.DIAMOND_BLOCK);
+        breadRecipe.setIngredient('C', Material.GOLD_BLOCK);
         return breadRecipe;
     }
 
@@ -107,6 +127,84 @@ public class Main extends JavaPlugin implements Listener {
         return fleshToRabbit;
     }
 
+    private ShapedRecipe compressedStoneRecipe() {
+        final String compressedStoneNbt = "compressed_stone";
+
+        NamespacedKey key = new NamespacedKey(this, compressedStoneNbt);
+        ItemStack result = new ItemStack(Material.STONE);
+        ItemMeta meta = result.getItemMeta();
+        meta.getPersistentDataContainer()
+            .set(new NamespacedKey(this, compressedStoneNbt), PersistentDataType.STRING, compressedStoneNbt);
+        meta.itemName(
+            serializer.deserialize("<#83909B>Compressed Stone")
+        );
+        result.setItemMeta(meta);
+        ShapedRecipe recipe = new ShapedRecipe(key, result);
+        recipe.shape("AAA", "AAA", "AAA");
+        recipe.setIngredient('A', Material.STONE);
+        return recipe;
+    }
+
+    private ShapedRecipe compressedCobblestoneRecipe() {
+        final String compressedStoneNbt = "compressed_cobblestone";
+
+        NamespacedKey key = new NamespacedKey(this, compressedStoneNbt);
+        ItemStack result = new ItemStack(Material.COBBLESTONE);
+        ItemMeta meta = result.getItemMeta();
+        meta.getPersistentDataContainer()
+            .set(new NamespacedKey(this, compressedStoneNbt), PersistentDataType.STRING, compressedStoneNbt);
+        meta.itemName(
+            serializer.deserialize("<#4F5E67>Compressed Cobblestone")
+        );
+        result.setItemMeta(meta);
+        ShapedRecipe recipe = new ShapedRecipe(key, result);
+        recipe.shape("AAA", "AAA", "AAA");
+        recipe.setIngredient('A', Material.COBBLESTONE);
+        return recipe;
+    }
+
+    private ShapelessRecipe decompressCobblestoneRecipe() {
+        NamespacedKey key = new NamespacedKey(this, "decompress_cobblestone");
+        ItemStack result = new ItemStack(Material.COBBLESTONE, 9);
+        ShapelessRecipe recipe = new ShapelessRecipe(key, result);
+        recipe.addIngredient(new RecipeChoice.ExactChoice(compressedCobblestoneRecipe().getResult()));
+        return recipe;
+    }
+    
+    private ShapelessRecipe decompressStoneRecipe() {
+        NamespacedKey key = new NamespacedKey(this, "decompress_stone");
+        ItemStack result = new ItemStack(Material.STONE, 9);
+        ShapelessRecipe recipe = new ShapelessRecipe(key, result);
+        recipe.addIngredient(new RecipeChoice.ExactChoice(compressedStoneRecipe().getResult()));
+        return recipe;
+    }
+
+    private ShapelessRecipe decompressDirtRecipe() {
+        NamespacedKey key = new NamespacedKey(this, "decompress_dirt");
+        ItemStack result = new ItemStack(Material.DIRT, 9);
+        ShapelessRecipe recipe = new ShapelessRecipe(key, result);
+        recipe.addIngredient(new RecipeChoice.ExactChoice(compressedDirtRecipe().getResult()));
+        return recipe;
+    }
+    
+    private ShapedRecipe compressedDirtRecipe() {
+        final String compressedDirtNbt = "compressed_dirt";
+
+        NamespacedKey key = new NamespacedKey(this, "compressed_dirt");
+        ItemStack result = new ItemStack(Material.DIRT);
+        ItemMeta meta = result.getItemMeta();
+        meta.getPersistentDataContainer()
+            .set(new NamespacedKey(this, compressedDirtNbt), PersistentDataType.STRING, compressedDirtNbt);
+        meta.itemName(
+            serializer.deserialize("<#A06A5A>Compressed Dirt")
+        );
+        result.setItemMeta(meta);
+        ShapedRecipe recipe = new ShapedRecipe(key, result);
+        recipe.shape("AAA", "AAA", "AAA");
+        recipe.setIngredient('A', Material.DIRT);
+        return recipe;
+    }
+
     @EventHandler
     public void onConsume(PlayerItemConsumeEvent event) {
         ItemStack item = event.getItem();
@@ -126,7 +224,7 @@ public class Main extends JavaPlugin implements Listener {
                 // TODO: set item cooldown
                 eater.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 3628, 3));
                 eater.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 511, 5));
-                eater.addPotionEffect(new PotionEffect(PotionEffectType.STRENGTH, 3628, 5));
+                eater.addPotionEffect(new PotionEffect(PotionEffectType.STRENGTH, 3628, 2));
                 eater.addPotionEffect(new PotionEffect(PotionEffectType.JUMP_BOOST, 3628, 2));
 
                 eater.setSaturation(20f);
