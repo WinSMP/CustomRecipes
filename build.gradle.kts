25a5c523d948bc3fd767174697391675d8ff0fea
winlogon <walker84837@gmail.com>
2026-01-18 16:35:05 +0100

    chore: update dependencies
    * deps: bump actions/checkout from 5 to 6
    * deps: bump actions/upload-artifact from 5 to 6
    * deps: bump com.gradleup.shadow from 9.2.2 to 9.3.1
    * deps: bump io.papermc.paper:paper-api
    * deps: bump org.junit.jupiter:junit-jupiter from 6.0.1 to 6.0.2
    * deps: bump org.junit.platform:junit-platform-launcher
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import java.util.Date
 import java.util.TimeZone
 
 plugins {
-    id("com.gradleup.shadow") version "9.2.2"
+    id("com.gradleup.shadow") version "9.3.1"
     id("java")
 }
 
@@ -68,10 +68,10 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.10-R0.1-SNAPSHOT")
-    testImplementation("io.papermc.paper:paper-api:1.21.10-R0.1-SNAPSHOT")
-    testImplementation("org.junit.jupiter:junit-jupiter:6.0.1")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher:6.0.1")
+    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+    testImplementation("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+    testImplementation("org.junit.jupiter:junit-jupiter:6.0.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:6.0.2")
 }
 
 tasks.test {
